cd7adb47ce21d9ef173b4fc581bcb89f41cb464b
DToila <diogo.t.candeias@gmail.com>
2026-02-28 23:16:42 +0000

    Update Astro config with site and output settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://eqp26.github.io',
+  base: '/sailingwearable',
+  output: 'static',
+});
