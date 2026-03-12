2d84bc2bba7789a042084d40c0f8cab569f1e01d
Miguel Duarte <miguelcotrimduarte@gmail.com>
2026-03-12 14:51:09 +0000

    adicção da vírgula

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://eqp26.github.io',
+  base: '/sailingwearable',
+  output: 'static',
+});
